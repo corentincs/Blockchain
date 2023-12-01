7345901697416629354de12e54eb499e3d9a2e8b
corentin <corentin.garreau@student-cs.fr>
2023-12-01 10:53:47 +0100

    Fonction comptage terminée

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,11 @@ import config
 from block import Block, InvalidBlock
 from transaction import Transaction
 import utils
+import copy
+import numpy
+
+class NoCandidatDesigned(Exception):
+    pass
 
 
 class Blockchain(object):
@@ -130,8 +135,48 @@ class Blockchain(object):
         for b in self.blocks:
             b.log()
     def comptage(self) : 
-        """renvoie un dictionaire avec l'enssemble des candidats et leur nombre de voie"""
-        comptage = {}
+        """renvoie le vainqueur du vote en utiisant la Méthode Condorcet avec rangement des paires par ordre décroissant"""
+        #on associe chaque candidat à un indice en utilisant le premier vote du dernier block
+        candidats = {}
+        for i in range(len(self.last_block.transactions[0].votes)) :
+            candidats[self.last_block.transactions[0].votes[i]] = i
+        #on créé une matrice de préférence entre les candidats qui compile les votes
+        comptage = [[0 for i in range(len(candidats))] for i in range(len(candidats))]
         for block in self.blocks : 
             for transaction in block : 
-                
+                votes = transaction.votes
+                for i in range(len(votes)) : 
+                    for j in range(i+1,len(votes)) : 
+                        comptage[candidats[votes[i]]][candidats[votes[j]]] +=1
+        
+        #On vérifie si un candidat est préféré à tout autre par une majorité 
+        for candidat in candidats.keys() : 
+            vainqueur = True 
+            for opposant in candidats.keys() :
+                if not(comptage[candidats[candidat]][candidats[opposant]] > comptage[candidats[opposant]][candidats[candidat]]) : vainqueur = False 
+            if vainqueur : return candidat
+        #Il y a paradoxe de Condorcet on le résoult en utilisant le rangement des paires par ordre décroissant
+
+        graph_condorcet = numpy.maximum(numpy.array(comptage - comptage.T,0))
+
+        def précence_cycle( matrice ) : 
+            mat = numpy.array(matrice)
+            power = numpy.linalg.matrix_power(mat, len(matrice))
+            for i in range(len(power)):
+                if power[i][i] > 0 : return True
+            return False 
+        #on traite les paires par orde décroissant
+        init = numpy.zeros((len(graph_condorcet),len(graph_condorcet)))
+        while max(graph_condorcet) != 0 : 
+            indice = numpy.argmax(graph_condorcet)
+            graph_condorcet[indice[0]][indice[1]] = 0
+            init[indice[0]][indice[1]] = 1 
+            if précence_cycle(init) : 
+                init[indice[0]][indice[1]] = 0
+        #designation du vainqueur
+        for candidat in candidats.keys() : 
+            vainqueur = True 
+            for opposant in candidats.keys() :
+                if init[candidats[opposant]][candidats[candidat]] > 0 : vainqueur = False 
+            if vainqueur : return candidat
+        raise NoCandidatDesigned 
